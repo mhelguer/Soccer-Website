e99b7399757659ef2ec33352673125a4359ff3ca
Miguel <miguelihelguero@gmail.com>
2023-12-21 00:04:09 -0600

    initial data showing, changeData button now working

--- app/routes/teams.js
+++ app/routes/teams.js
@@ -7,16 +7,12 @@ import fetch from 'fetch';
 
 export default class TeamsRoute extends Route {
 
-  //FIXME: this.data is not appearing when page first loaded up
   async model() {
     const response = await fetch('http://localhost:3000/api/data/teams/1');
     const resp = await response.json();
     this.data = resp;
-    console.log(this.data.data);
-    // return fetch('http://localhost:3000/api/data/teams/1')
-    // .then(response => response.json);
+    console.log('model this.data.data: ',this.data.data);  //[{...}, {...}, {...}, {...}] div 1
 
-
-    return resp;
+    return this.data;
   }
 }
